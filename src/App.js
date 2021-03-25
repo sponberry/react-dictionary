03ab85ac694b151fca644750743f811dcedb1b4a
sponberry <74988392+sponberry@users.noreply.github.com>
2021-03-25 14:15:53 +0000

    Added attribution link to header in top right corner

--- src/App.js
+++ src/App.js
@@ -7,18 +7,27 @@ import './App.css';
 function App() {
   let [bgImage, setBgImage] = useState({photographer: "Snapwire", src: {large2x:"https://images.pexels.com/photos/6997/books-writing-reading-sonja-langford.jpg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"}})
 
+  const link = "https://github.com/sponberry/react-dictionary"
   const styles = {
     backgroundImage: 'url('+bgImage.src.large2x+')',
     backgroundSize: "cover",
     backgroundPosition: "center",
     height: "200vh",
-    opacity: 0.8,
+    opacity: 0.75,
   }
 
   return (
     <div className="App container-fluid" style={styles}>
         <header className="App-header">
           Image credit: {bgImage.photographer}
+        </header>
+        <header className="code-link row">
+            <div className="col-sm-6 mr-n2 icon">
+            <a href={link} target="_blank" rel="noreferrer">👩🏻‍💻</a>
+            </div>
+            <div className="col-sm-6 ml-n2 d-none d-sm-block">
+              <p className="p-header"><a href={link} target="_blank" rel="noreferrer">Code by Abi</a></p>
+            </div>
         </header>
         <DictionarySearch setPhotoData={(value) => {
           setBgImage(value);
